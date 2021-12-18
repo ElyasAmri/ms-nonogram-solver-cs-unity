5bed5d96a43f82907e0f2ac9bf0fd31440196eba
ElyasAmri <elyas.amri@gmail.com>
2021-12-18 23:14:22 +0300

    Fully implement Resolve function

--- Assets/Line.cs
+++ Assets/Line.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Line
 {
@@ -7,7 +8,7 @@ public class Line
 	Cell[] cells;
 	int length;
 
-	List<int[]> possibilities;
+	List<bool[]> possibilities;
 
 	public Line(Cell[] cells, ClueLine clues)
 	{
@@ -15,20 +16,32 @@ public class Line
 		this.cells = cells;
 		this.length = cells.Length;
 
-		possibilities = new List<int[]>();
+		possibilities = new List<bool[]>();
 		// TODO: make permutations from a utility
 	}
 
 	public int[] Resolve()
 	{
 		List<int> ret = new List<int>();
+
+		// A simple measure to skip rolling all of the cells for nothing
+		if (cells.All(cell => !cell.value.HasValue))
+			goto skipRemoval;
 		
 		// Remove failed possibilities
 		possibilities.RemoveAll(poss =>
 		{
-			// TODO: check the validity of the possibility
+			for (var i = 0; i < length; i++)
+			{
+				// if the cell has been determined and it doesn't match its
+				// respective position in the possibility then remove it
+				if (cells[i].value.HasValue && poss[i] != cells[i].value.Value)
+					return true;
+			}
 			return false;
 		});
+		
+		skipRemoval: ;
 		
 		// Determine if there is a new cell after the removal
 		for (var i = 0; i < length; i++)
@@ -38,19 +51,11 @@ public class Line
 				continue;
 			
 			// check if all possibilities for one cell are true
-			if (possibilities.TrueForAll(poss =>
-			{
-				// TODO: check for all true
-				return true;
-			})) 
+			if (possibilities.TrueForAll(poss => poss[i])) 
 				cells[i].value = true;
 			
 			// check if all possibilities for one cell are false
-			else if (possibilities.TrueForAll(poss =>
-			{
-				// TODO: check for all false
-				return true;
-			}))
+			else if (possibilities.TrueForAll(poss => !poss[i]))
 				cells[i].value = false;
 			
 			// Failed to find such case
