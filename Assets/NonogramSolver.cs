b35b0184e12be4618a8844bc11e751754919aa4a
ElyasAmri <elyas.amri@gmail.com>
2021-12-20 21:56:46 +0300

    Separate one step and multi-step solve options

--- Assets/NonogramSolver.cs
+++ Assets/NonogramSolver.cs
@@ -15,16 +15,26 @@ public class NonogramSolver : MonoBehaviour
     [SerializeField] GameObject emptyBlock;
     [SerializeField] GameObject unknownBlock;
     [SerializeField] GameObject clueBlock;
+
+    [Header("Step Solving")]
+    [SerializeField] bool stepSolvingEnabled = false;
     [SerializeField] float frameTime = 0.25f;
+
+    float rows;
+    float cols;
+    float offsetX;
+    float offsetY;
+
+    void Awake()
+    {
+        rows = hClues.Count;
+        cols = vClues.Count;
+        offsetX = cols / 2f - 0.5F;
+        offsetY = rows / 2f - 0.5F;
+    }
 
     void OnDrawGizmos()
     {
-        var rows = hClues.Count;
-        var cols = vClues.Count;
-        var offsetX = cols / 2f - 0.5F;
-        var offsetY = rows / 2f - 0.5F;
-
-        
         Vector3 min = Vector3.one * 0.9f; 
         Vector3 small = Vector3.one * 0.8f; 
         
@@ -47,12 +57,48 @@ public class NonogramSolver : MonoBehaviour
         }
     }
 
-    IEnumerator Start()
+    void Start()
     {
-        var rows = hClues.Count;
-        var cols = vClues.Count;
-        var offsetX = cols / 2f - 0.5F;
-        var offsetY = rows / 2f - 0.5F;
+        DrawCanvas();
+        if (stepSolvingEnabled)
+        {
+            StartCoroutine(MultiStepSolve());
+        }
+        else
+        {
+            OneStepSolve();
+        }
+    }
+
+    IEnumerator MultiStepSolve()
+    {
+        Board board = new Board(hClues, vClues);
+        var waitTime = new WaitForSeconds(frameTime);
+        foreach (var newCells in board.SolveSteps())
+        {
+            foreach (var (i, j, s) in newCells)
+            {
+                CreateBlock(new Vector3(j - offsetX, rows - i - 1 - offsetY), GetTypeFromBool(s));
+                yield return waitTime;
+            }
+        }
+    }
+
+    void OneStepSolve()
+    {
+        Board board = new Board(hClues, vClues);
+        board.Solve();
+        var result = board.GetResult();
+
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+                CreateBlock(new Vector3(j - offsetX, rows - i - 1 - offsetY), GetTypeFromBool(result[(i, j)].value));
+        }
+    }
+
+    void DrawCanvas()
+    {
         for (var i = 0; i < rows; i++)
         {
             for (var x = 0; x < hClues[i].values.Count; x++)
@@ -68,45 +114,34 @@ public class NonogramSolver : MonoBehaviour
                 CreateClue(new Vector3(i - offsetX, y - offsetY + rows), vClues[i].values[y]);
             }
         }
-        
-        Board board = new Board(hClues, vClues);
-        var waitTime = new WaitForSeconds(frameTime);
-        foreach (var newCells in board.SolveSteps())
-        {
-            foreach (var (i, j, s) in newCells)
-            {
-                CreateBlock(new Vector3(j - offsetX, rows - i - 1 - offsetY), GetTypeFromBool(s));
-                yield return waitTime;
-            }
-        }
+    }
 
-        BlockType GetTypeFromBool(bool? b)
+    BlockType GetTypeFromBool(bool? b)
+    {
+        return b switch
         {
-            return b switch
-            {
-                true => BlockType.Full,
-                false => BlockType.None,
-                null => BlockType.Unknown
-            };
-        }
+            true => BlockType.Full,
+            false => BlockType.None,
+            null => BlockType.Unknown
+        };
+    }
         
-        void CreateBlock(Vector3 position, BlockType type)
+    void CreateBlock(Vector3 position, BlockType type)
+    {
+        Instantiate(type switch
         {
-            Instantiate(type switch
-                {
-                    BlockType.Clue => clueBlock,
-                    BlockType.Full => fullBlock,
-                    BlockType.None => emptyBlock,
-                    BlockType.Unknown => unknownBlock,
-                    _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-                }, position, Quaternion.identity, transform);
-        }
+            BlockType.Clue => clueBlock,
+            BlockType.Full => fullBlock,
+            BlockType.None => emptyBlock,
+            BlockType.Unknown => unknownBlock,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        }, position, Quaternion.identity, transform);
+    }
 
-        void CreateClue(Vector3 position, int value)
-        {
-            Instantiate(clueBlock, position, Quaternion.identity, transform)
-                .GetComponentInChildren<Text>().text = value.ToString();
-        }
+    void CreateClue(Vector3 position, int value)
+    {
+        Instantiate(clueBlock, position, Quaternion.identity, transform)
+            .GetComponentInChildren<Text>().text = value.ToString();
     }
 }
 
